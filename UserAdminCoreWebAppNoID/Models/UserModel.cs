b93f30d81a86fa54500ea5287c33505a7b8f7387
Iftikhar Siddique <robustsystems@hotmail.co.uk>
2020-09-28 15:47:12 +0100

    Added project UserAdminCoreWebAppNoID.

---
+++ UserAdminCoreWebAppNoID/Models/UserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UserAdminCoreWebAppNoID.Models
+{
+    public class UserModel
+    {
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "You need to give us your email address.")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "You must have a password.")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "You need to provide a long enough password.")]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Your password and confirm password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
